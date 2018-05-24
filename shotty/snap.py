841f2b30f86a17366a0228dbe35499826f676982
Héctor Esteban <hesteban@tcpsi.es>
2018-05-24 18:22:34 +0200

    second commit

--- shotty/snap.py
+++ shotty/snap.py
@@ -1,10 +1,35 @@
 import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+@click.command()
+@click.option('--project', default=None,
+               help="Only instaces for project (tag Project:<nam>)")
+def list_instances(project):
+    "List EC2 Instances"
+    instances =[]
+    if project is not None:
+        filters = [{'Name': 'tag:Project', 'Values': [project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    for i in instances:
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name)))
+
+    return
 
 if __name__ == '__main__':
 
-	session = boto3.Session(profile_name='shotty')
-	ec2 = session.resource('ec2')
+    list_instances()
 
 
-	for vm in ec2.instances.all():
-		print(vm)
+
+
